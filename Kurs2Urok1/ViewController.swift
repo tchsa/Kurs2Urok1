47a6c63f0d7118a7821f2f119ee253a3be34989c
MSP <mit_sergey@mail.ru>
2024-11-28 14:17:49 +0700

    Initial Commit

---
+++ Kurs2Urok1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kurs2Urok1
+//
+//  Created by Сергей Митев on 28.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
